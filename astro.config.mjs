6a6453021c2b14af7b303dc101b4486dc092a350
Kaiba <yt.kaibaoke@gmail.com>
2026-05-30 11:20:00 +0900

    chore: update site URL to pmf.karmait.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://pmflow.karmait.net',
+  site: 'https://pmf.karmait.net',
   integrations: [
     mdx(),
     sitemap(),
